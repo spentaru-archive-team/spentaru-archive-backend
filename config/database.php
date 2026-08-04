b3231814bc131cddee1c45913b11693a985dc6a1
Falrafa4 <rafaasad9@gmail.com>
2026-08-04 07:49:36 +0700

    fix: fallback DB_CONNECTION from default to mysql

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => (env('DB_CONNECTION') === 'default' || !env('DB_CONNECTION')) ? 'mysql' : env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
